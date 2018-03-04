34fea4077e0409566a4332e8d7fab10dcfb179ff
kt-ujwal <kt.ujwal@gmail.com>
2018-03-04 10:13:34 +0530

    linear regression on prima indian diabetes dataset

--- diabetes_lr.py
+++ diabetes_lr.py
@@ -11,13 +11,14 @@ from sklearn.metrics import mean_squared_error,r2_score
 url="http://goo.gl/j0Rvxq"
 raw_data=urllib.urlopen(url)
 df=np.loadtxt(raw_data,delimiter=",")
-#Dat = np.genfromtxt(df, names=True)
-#print(Dat)
+#df=np.array
 print(df.shape)
 #df.fillna(-9999,inplace=True)
 X= df[:,0:8]
+
 #X = df.data[:, np.newaxis, 2]
 y=df[:,8]
+#y = np.reshape(X, (-1, 1))
 X=np.delete(X,2,1)
 X=preprocessing.scale(X)
 #print X.feature_names
@@ -34,21 +35,22 @@ accuracy=model.score(X_test,y_test)
 print(accuracy)
 
 y_pred=model.predict(X_test)
-
 print('Coefficients:\n',model.coef_)
 
 print("Mean squared error: %.2f"
       % mean_squared_error(y_test, y_pred))
 
 print('Variance score: %.2f' % r2_score(y_test, y_pred))
+X_test=X_test[:, 0]
+X_train=X_train[:, 0]
 print(X_test.shape)
 print(y_test.shape)
-
-plt.scatter(X_test,y_test,c='red')
+#X1_test=X_test.flatten()
+plt.scatter(X_train,y_train,c='red')
 plt.xlabel("features")
 plt.ylabel("malign/beningn")
 plt.title("diabetes linear regression")
-plt.plot(X_test,y_pred,c='black',linewidths=2)
+plt.plot(X_test,y_pred,c='black',linewidth=2)
 
 plt.show()
 
